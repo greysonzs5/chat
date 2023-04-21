653c18b6be0df4fd46bf9f257ee3a398e43dfa75
Greysonzs <127030540+Greysonzs@users.noreply.github.com>
2023-04-21 21:41:08 +0800

    Update entry-client.tsx

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,7 @@
 import { mount, StartClient } from "solid-start/entry-client"
 import { LocalStorageKey } from "~/types"
 
+
 const e = localStorage.getItem(LocalStorageKey.THEME) || ""
 const a = window.matchMedia("(prefers-color-scheme: dark)").matches
 if (!e || e === "auto" ? a : e === "dark") {
@@ -17,3 +18,11 @@ if (!Array.prototype.at) {
 }
 
 mount(() => <StartClient />, document)
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5aff25b20308b19618d1ea0a4797216b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
