06d97625fd66177e1b36c1c53f965973343c8555
Greysonzs <127030540+Greysonzs@users.noreply.github.com>
2023-04-21 12:45:10 +0800

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -18,12 +18,15 @@ export const defaultEnv = {
     continuousDialogue: true,
     APIModel: "gpt-3.5-turbo" as Model
   },
-  CLIENT_DEFAULT_MESSAGE: `Powered by OpenAI Vercel
-- 如果本项目对你有所帮助，可以给小猫 [买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
-- 本网站仅作为项目演示，不提供服务，请填入自己的 Key，长期使用请 [自行部署](https://github.com/ourongxing/chatgpt-vercel#%E9%83%A8%E7%BD%B2%E4%B8%80%E4%B8%AA%E4%BD%A0%E8%87%AA%E5%B7%B1%E7%9A%84-chatgpt-%E7%BD%91%E7%AB%99%E5%85%8D%E8%B4%B9)，简单成本低。
-- 点击每条消息前的头像，可以锁定对话，作为角色设定。[查看更多使用技巧](https://github.com/ourongxing/chatgpt-vercel#使用技巧)。
-- 现在支持多个对话，打开对话设置，点击新建对话。在输入框里输入 [[/]][[/]] 或者 [[空格]][[空格]] 可以切换对话，搜索历史消息。
-- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。
+  CLIENT_DEFAULT_MESSAGE: `- [[Shift]] + [[Enter]] 换行。本站完全免费无次数限制，如果可以希望大家多多分享给身边的朋友给支持，谢谢！！
+- 为了自己的安全着想请各位千千万万不要提一些敏感词，已经有案例了！！！
+- 使用问题或交流讨论GPT4.0可以[加入QQ群](https://jq.qq.com/?_wv=1027&k=a7IXPtfD)
+- 如果本项目对你有帮助，可以给老弟[买瓶可乐](https://www.lianmenghao.cn/reward.png)
+- ChatBox战略合作伙伴公众号有学Python大数据的可以关注一个波[信息技术智能库](https://app.yinxiang.com/fx/13ce6bbd-f36f-4e92-be53-92dd381ed729)
+- 版本更新了想快速熟悉更多细节功能可以[点我查看玩转教程](https://docs.qq.com/doc/DT093aUJRS3hYQ2lw)
+- 免费时间现在改为12-22点需要全时段使用可以[点我购买API](https://www.lianmenghao.cn/zb_users/upload/2023/03/20230327141734167989785492356.jpg)
+- 对画图有需要的用户可以[点我访问独享版](https://ctgpt2.xyz)
+- [安卓端下载](https://wwji.lanzouf.com/i2gyT0qz28he) | [IOS端下载](https://wwji.lanzouf.com/i7bJz0tcb69i)
 `,
   CLIENT_MAX_INPUT_TOKENS: {
     "gpt-3.5-turbo": 4 * 1024,
